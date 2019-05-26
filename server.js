c2b9a6d1a13a24df47ac57c8d15330f4e39b7d3a
Zachary Bray <zachbray@MBP.local>
2019-05-26 13:58:19 -0500

    adding old changes before environ change

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
+
+//this solution for heroku deployment sourced from https://medium.com/jeremy-gottfrieds-tech-blog/tutorial-how-to-deploy-a-production-react-app-to-heroku-c4831dfcfa08
